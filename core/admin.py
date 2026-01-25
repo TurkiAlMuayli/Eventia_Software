210f560024b04919781c3b86c01941fa1b79afbe
Abdullah Alshammari <abdu114hf16@gmail.com>
2026-01-25 12:08:27 +0300

    Fix admin to use CustomUser

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, OrganizerProfile, VendorProfile, Event, VendorApplication
+from .models import CustomUser, OrganizerProfile, AttendeeProfile
 
-admin.site.register(User, UserAdmin)
+# Register your CustomUser so you can see it in the admin panel
+admin.site.register(CustomUser, UserAdmin)
 
+# Register the profiles
 admin.site.register(OrganizerProfile)
-admin.site.register(VendorProfile)
-admin.site.register(Event)
-admin.site.register(VendorApplication)
+admin.site.register(AttendeeProfile)
