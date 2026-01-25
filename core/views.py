398df6f894625b495cc8edc39bd9bc8cb803bdcc
Abdullah Alshammari <abdu114hf16@gmail.com>
2026-01-25 13:34:52 +0300

    Allow GET and PATCH for profile

--- core/views.py
+++ core/views.py
@@ -1,25 +1,35 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .models import CustomUser
 from .serializers import OrganizerProfileSerializer, AttendeeProfileSerializer
 
 
-@api_view(['PATCH'])
+@api_view(['GET', 'PATCH'])
 @permission_classes([IsAuthenticated])
 def update_profile(request):
     user = request.user
 
+    instance = None
+    SerializerClass = None
+
     if user.role == 'ORGANIZER' and hasattr(user, 'organizer_profile'):
-        serializer = OrganizerProfileSerializer(user.organizer_profile, data=request.data, partial=True)
-
+        instance = user.organizer_profile
+        SerializerClass = OrganizerProfileSerializer
     elif user.role == 'ATTENDEE' and hasattr(user, 'attendee_profile'):
-        serializer = AttendeeProfileSerializer(user.attendee_profile, data=request.data, partial=True)
-
+        instance = user.attendee_profile
+        SerializerClass = AttendeeProfileSerializer
     else:
         return Response({"error": "No specific profile found for this role"}, status=400)
 
-    if serializer.is_valid():
-        serializer.save()
+    if request.method == 'GET':
+        serializer = SerializerClass(instance)
         return Response(serializer.data)
-    return Response(serializer.errors, status=400)
+
+    elif request.method == 'PATCH':
+        serializer = SerializerClass(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    return Response(status=405)
