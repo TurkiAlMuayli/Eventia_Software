21440b5f1c1e7e2a762de31210e2292cd9c26665
Abdullah Alshammari <abdu114hf16@gmail.com>
2026-01-28 21:47:59 +0300

    frontend team edits

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, OrganizerProfile, AttendeeProfile
+from .models import CustomUser, OrganizerProfile, VendorProfile, AttendeeProfile
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'role', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('role', 'phone_number')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(OrganizerProfile)
+admin.site.register(VendorProfile)
 admin.site.register(AttendeeProfile)
