c05b252df37340703f6a1facb99f287d3ec5e4a7
Abdullah Alshammari <abdu114hf16@gmail.com>
2026-01-28 17:32:22 +0300

    frontend team edits

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_view, name='dashboard'),
+    path('', views.dashboard_view, name='index'),
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
