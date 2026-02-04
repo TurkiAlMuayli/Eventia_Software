c8d8873fadf70914a2aa7b9ab09effea5517f8a0
Abdullah Alshammari <abdu114hf16@gmail.com>
2026-02-04 18:23:07 +0300

    redirections updates

--- core/urls.py
+++ core/urls.py
@@ -2,14 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 1. Add this Empty Path for the Homepage
+    path('', views.dashboard_view, name='home'),
+
     # Dashboard
     path('dashboard/', views.dashboard_view, name='dashboard'),
 
-    # Attendee Routes (Default)
+    # Attendee Routes
     path('signup/', views.signup_attendee, name='signup_attendee'),
     path('login/', views.login_attendee, name='login_attendee'),
 
-    # Business Routes (Organizer/Vendor)
+    # Business Routes
     path('signup/business/', views.signup_business, name='signup_business'),
     path('login/business/', views.login_business, name='login_business'),
 ]
